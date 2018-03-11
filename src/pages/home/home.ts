ae5f6a3c7f13426b650b14b8dc7945acc5f88a7d
arllain <arllain@gmail.com>
2018-03-11 01:12:52 -0300

    rest api get products

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import * as WC from 'woocommerce-api';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,22 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  WooCommerce : any;
 
+  constructor(public navCtrl: NavController) {
+    this.WooCommerce = WC({
+      url: "http://localhost/agcs/",
+      consumerKey:"ck_db5387a79cf293a5f5600bbf12be278d1652cd83",
+      consumerSecret:"cs_6fc7884f33d5048a29fe9f21460288e1478625cc",
+      version: 'wc/v2',
+      wpAPI: true
+    });
+
+    this.WooCommerce.getAsync("products").then((data)=>{
+      console.log(JSON.parse(data.body));
+    }, (err) => {
+      console.log(err);
+    })
   }
 
 }
